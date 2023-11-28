d796f5a01dabed2dd36aad53bfa483704c52c20b
Crowdbotics <engineering@crowdbotics.com>
2023-11-28 11:05:13 +0000

    Update store.

---
+++ store/mithileshqpppAPI/api.js
@@ -0,0 +1,95 @@
+import axios from "axios"
+const mithileshqpppAPI = axios.create({
+  baseURL: "https://mithileshqppp-2.botics.co",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_docs_schema_retrieve(payload) {
+  return mithileshqpppAPI.get(`/api-docs/schema/`, {
+    params: { lang: payload.lang }
+  })
+}
+function api_v1_login_create(payload) {
+  return mithileshqpppAPI.post(`/api/v1/login/`, payload)
+}
+function api_v1_signup_create(payload) {
+  return mithileshqpppAPI.post(`/api/v1/signup/`, payload)
+}
+function modules_articles_article_list(payload) {
+  return mithileshqpppAPI.get(`/modules/articles/article/`)
+}
+function modules_articles_article_create(payload) {
+  return mithileshqpppAPI.post(`/modules/articles/article/`, payload)
+}
+function modules_articles_article_retrieve(payload) {
+  return mithileshqpppAPI.get(`/modules/articles/article/${payload.id}/`)
+}
+function modules_articles_article_update(payload) {
+  return mithileshqpppAPI.put(
+    `/modules/articles/article/${payload.id}/`,
+    payload
+  )
+}
+function modules_articles_article_partial_update(payload) {
+  return mithileshqpppAPI.patch(
+    `/modules/articles/article/${payload.id}/`,
+    payload
+  )
+}
+function modules_articles_article_destroy(payload) {
+  return mithileshqpppAPI.delete(`/modules/articles/article/${payload.id}/`)
+}
+function rest_auth_login_create(payload) {
+  return mithileshqpppAPI.post(`/rest-auth/login/`, payload)
+}
+function rest_auth_logout_create(payload) {
+  return mithileshqpppAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(payload) {
+  return mithileshqpppAPI.post(`/rest-auth/password/change/`, payload)
+}
+function rest_auth_password_reset_create(payload) {
+  return mithileshqpppAPI.post(`/rest-auth/password/reset/`, payload)
+}
+function rest_auth_password_reset_confirm_create(payload) {
+  return mithileshqpppAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+}
+function rest_auth_registration_create(payload) {
+  return mithileshqpppAPI.post(`/rest-auth/registration/`, payload)
+}
+function rest_auth_registration_resend_email_create(payload) {
+  return mithileshqpppAPI.post(`/rest-auth/registration/resend-email/`, payload)
+}
+function rest_auth_registration_verify_email_create(payload) {
+  return mithileshqpppAPI.post(`/rest-auth/registration/verify-email/`, payload)
+}
+function rest_auth_user_retrieve(payload) {
+  return mithileshqpppAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return mithileshqpppAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return mithileshqpppAPI.patch(`/rest-auth/user/`, payload)
+}
+export const apiService = {
+  api_docs_schema_retrieve,
+  api_v1_login_create,
+  api_v1_signup_create,
+  modules_articles_article_list,
+  modules_articles_article_create,
+  modules_articles_article_retrieve,
+  modules_articles_article_update,
+  modules_articles_article_partial_update,
+  modules_articles_article_destroy,
+  rest_auth_login_create,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_resend_email_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
